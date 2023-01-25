4ccfde7de7e7b29562d092651281a42c4942094b
Maker Athian <makerathian@gmail.com>
2023-01-26 00:54:43 +0200

    Complete REWIND coding challenge

--- js/site.js
+++ js/site.js
@@ -1,4 +1,31 @@
-// Call Hello World
-function HelloWorld(){
-    alert("Hello World");
+// Get the String from the page.
+// Controll function
+function getValue(){
+    document.getElementById("alert").classList.add("invisible");
+    let userString = document.getElementById("userString").value;
+    let revString = reverseString(userString);
+    displayString(revString);
+}
+
+//Reverse the string
+// Logic function.
+function reverseString(userString){
+    // Create empty array.
+    let revString = [];
+
+    // reverse a string using a for loop.
+    for (let index = userString.length - 1; index >= 0; index--){
+        revString += userString[index];
+    }
+    // Return reverse string.
+    return revString;
+}
+
+//Display the reverse string to the user.
+// View Function.
+function displayString(revstring){
+    //Write message to the page
+    document.getElementById("msg").innerHTML = `Your string reversed is: ${revstring}`;
+    //Show the alert box
+    document.getElementById("alert").classList.remove("invisible");
 }
